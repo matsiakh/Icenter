814917c5123af1f687f32c40f3179e360167b504
matsiakh <665lucifer1@gmail.com>
2023-05-17 00:09:29 +0300

    swiper cdn

--- app/js/app.js
+++ app/js/app.js
@@ -1,31 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-
 	//------------------------------SLIDER---------------------------
 	var swiper = new Swiper(".mySwiper", {
 		freeMode: false,
 		loop: true,
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
-		},
-		navigation: {
-			nextEl: '.present__next',
-			prevEl: '.present__prev',
-		},
-		breakpoints: {
-			320: {
-				slidesPerView: 1,
-				spaceBetween: 15
-			},
-			480: {
-				slidesPerView: 2,
-				spaceBetween: 30
-			},
-			900: {
-				slidesPerView: 3,
-				spaceBetween: 30
-			}
-		},
+		effect: "fade",
 	});
 
 });
