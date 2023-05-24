4211cb6727b4d1a6dc82ce01d15733792e79879a
matsiakh <665lucifer1@gmail.com>
2023-05-24 23:59:54 +0300

    conditions slide

--- app/js/app.js
+++ app/js/app.js
@@ -6,5 +6,21 @@ document.addEventListener("DOMContentLoaded", function() {
 		effect: "fade",
 	});
 
+	function changeTab(index) {
+		// Отримати всі елементи табів та слайдів
+		var tabs = document.getElementsByClassName('tab');
+		var slides = document.getElementsByClassName('slide');
+	
+		// Зняти активний клас з усіх табів та слайдів
+		for (var i = 0; i < tabs.length; i++) {
+			tabs[i].classList.remove('active');
+			slides[i].classList.remove('active');
+		}
+	
+		// Додати активний клас до обраного табу та слайда
+		tabs[index].classList.add('active');
+		slides[index].classList.add('active');
+	}
+
 });
 	
